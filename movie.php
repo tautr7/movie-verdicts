045e6886e716c04b4c92b4e33f36b827ad4afa40
tautr7 <163383247+tautr7@users.noreply.github.com>
2024-12-02 18:15:08 +1100

    Update movie.php

--- movie.php
+++ movie.php
@@ -22,6 +22,9 @@ if (!$movie) {
 }
 
 include __DIR__ . '/includes/templates/header.php';
+
+echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">';
+
 ?>
 
 <div class="container">
